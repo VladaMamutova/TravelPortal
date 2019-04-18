ed34284f30421afc0154adb2362d06fccf9521f9
VladaMamutova <vlada.mamutova@gmail.com>
2019-04-19 01:25:54 +0300

    feat: add Dictionaries Page
    Created interface for tables-dictionaries:
    - included the Dragablz package for dragable and tearable tab controls for WPF;
    - added vertical tab menu for dictionaries;
    - added icons for each tab item;
    - added interface prototype markup for dictionary management.

---
+++ TravelPortal/Views/DictionariesPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TravelPortal.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для DictionariesPage.xaml
+    /// </summary>
+    public partial class DictionariesPage : Page
+    {
+        public DictionariesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
